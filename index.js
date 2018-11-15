0198c8f88f3540b028e8d6ea80036789bbde7357
Ted Sazon <tedsazon@gmail.com>
2018-11-15 10:01:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
   var kittenString = 'Ralph'
 // Add your functions and code here
+function destructivelyAppendKitten(kittenString) {
+  return [kittens, ...kittenString]
+}
